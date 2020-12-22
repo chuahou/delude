db3392ff8c7d55f81eec638c7becea4ae4ca0a35
Chua Hou <human+github@chuahou.dev>
2020-12-22 16:34:43 +0800

    nix: use 'filterSource' to exclude noise from build
    - Better caching
    - Less rebuilds

--- default.nix
+++ default.nix
@@ -1,3 +1,13 @@
 { pkgs ? import ./nix/nixpkgs.nix {} }:
 
-pkgs.haskellPackages.callCabal2nix "delude" ./. {}
+let
+  src = builtins.filterSource (path: type:
+    (type != "directory" || baseNameOf path != ".dist-newstyle") &&
+    (type != "symlink"   || baseNameOf path != "result")         &&
+    (type != "directory" || baseNameOf path != ".github")        &&
+    (type != "directory" || baseNameOf path != ".git"))          ./.;
+
+in
+  builtins.deepSeq
+    (builtins.readFile ./package.yaml)
+    (pkgs.haskellPackages.callCabal2nix "delude" src {})
